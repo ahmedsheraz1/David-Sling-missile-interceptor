014283b41e01fc7df374d8dd9878ecd5547bc6cc
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-12 10:04:11 +0500

    Create ranges.py

---
+++ ranges.py
@@ -0,0 +1,6 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+xvec = [10, 20, 30, 40]
+yvec = [7, 5, 3, 1]
+sum(x*y for x,y in zip(xvec, yvec))
+300
