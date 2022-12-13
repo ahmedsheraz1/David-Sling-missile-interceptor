d1e6d78adb9a5d99eed4c2bb143d0c880ee3741c
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-14 02:39:54 +0500

    Create David Sling missile.py

---
+++ David Sling missile.py
@@ -0,0 +1,5 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+from hashlib import blake2b
+blake2b(b'David Sling missile').hexdigest()
+'9ea88f5eaad18120e20ecbe1a7e9c9fbf447a1b2c48e8d8ca8367898d7f1a9e4b03733a3115abb098a39d704c4dfe375814b893056c4dba746e04b33421ec708'
